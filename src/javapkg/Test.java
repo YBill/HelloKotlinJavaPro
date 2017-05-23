fe5fd4942201f901daf420a039416c96193b08b8
weibiao <ybilland@163.com>
2017-05-23 16:00:45 +0800

    Initial commit

---
+++ src/javapkg/Test.java
@@ -0,0 +1,10 @@
+package javapkg;
+
+/**
+ * Created by Bill on 2017/5/23.
+ */
+public class Test {
+    public static void main(String args[]) {
+        System.out.println("Hello Java!");
+    }
+}
---
+++ src/ktpkg/Test.kt
@@ -0,0 +1,8 @@
+package ktpkg
+
+/**
+ * Created by Bill on 2017/5/23.
+ */
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+}
